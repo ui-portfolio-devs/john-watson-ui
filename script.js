f050829d98e69f14bdd24b8fc1bb572f3cbcfc47
felix-atoma <felixatoma2@gmail.com>
2025-04-25 13:00:22 +0000

    feat(hero): add hero section with image and type animation

---
+++ script.js
@@ -0,0 +1,76 @@
+const themeToggle = document.getElementById("themeToggle");
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+const dropdown = document.querySelector(".dropdown");
+
+const searchBox = document.getElementById("searchBox");
+const searchIcon = document.getElementById("searchIcon");
+
+let searchOpen = false;
+
+searchIcon.addEventListener("click", () => {
+  searchBox.classList.toggle("active");
+  searchOpen = !searchOpen;
+
+  if (searchOpen) {
+    searchIcon.classList.remove("fa-magnifying-glass");
+    searchIcon.classList.add("fa-xmark"); // close icon
+  } else {
+    searchIcon.classList.remove("fa-xmark");
+    searchIcon.classList.add("fa-magnifying-glass");
+  }
+});
+
+// Theme toggle
+themeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("light");
+
+  if (document.body.classList.contains("light")) {
+    themeToggle.classList.remove("fa-moon");
+    themeToggle.classList.add("fa-sun");
+  } else {
+    themeToggle.classList.remove("fa-sun");
+    themeToggle.classList.add("fa-moon");
+  }
+});
+
+// Hamburger toggle
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  hamburger.classList.toggle("active");
+});
+
+// Dropdown for mobile
+dropdown.addEventListener("click", () => {
+  if (window.innerWidth <= 768) {
+    dropdown.classList.toggle("open");
+  }
+});
+
+// Hero Section
+const typingElement = document.getElementById("typing");
+const texts = ["Frontend Developer", "Programmer", "Tech Enthusiast"];
+let textIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function type() {
+  const currentText = texts[textIndex];
+  const displayText = currentText.substring(0, charIndex);
+  typingElement.textContent = displayText;
+
+  if (!isDeleting && charIndex < currentText.length) {
+    charIndex++;
+    setTimeout(type, 100);
+  } else if (isDeleting && charIndex > 0) {
+    charIndex--;
+    setTimeout(type, 50);
+  } else {
+    isDeleting = !isDeleting;
+    if (!isDeleting) textIndex = (textIndex + 1) % texts.length;
+    setTimeout(type, 1000);
+  }
+}
+
+type();
+
